e9984fac85f62441772e55ae41756b67c6ecf4ca
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-14 22:25:12 +0530

    Add files via upload

---
+++ common_errors_in_python.py
@@ -0,0 +1,32 @@
+'''diff typesof errors in python'''
+
+# a= 10
+# print(aa)                                                                      #NameError
+
+# b= 'hii'
+# print(int(b))                                                                  # ValueError
+
+
+# for x in range(0,5)
+# print(x)                                                                       #SyntaxError
+
+# v= 'hello'
+# f = 3.14
+# print(v+f)                                                                     # TypeError
+
+
+# l1 = [1,2,3,6,5,4]
+# print(l1[15])                                                                  # IndexError
+
+
+# print(10/0)                                                                    # ZeroDivisionError
+
+
+# for t in range(10):
+# print(t)                                                                       # IndentationError
+
+
+# s1 = 'hello',1,2,'python',3.14
+# print(s1.sort())                                                               # AttributeError
+
+
